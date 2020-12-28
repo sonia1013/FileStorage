47e6b2359f7f5a6b894190de21632620a0ddbfe6
sonia1013 <71934074+sonia1013@users.noreply.github.com>
2020-12-28 18:03:05 +0800

    Add files via upload

---
+++ LOL/Modules/helpers copy.js
@@ -0,0 +1,26 @@
+function $g(selector) {
+  if (selector.includes("#") && !selector.includes(" ")) {
+    //回傳Element
+    return document.querySelector(selector);
+  }
+
+  //回傳NodeList集合
+  let nodeList=document.querySelectorAll(selector);
+
+  return nodeList.length==1?nodeList[0]:nodeList;
+
+  function $c(selector) {
+    return document.createElement(selector);
+  }
+}
+
+  function $log(value) {
+    console.log(value);
+  }
+
+  function name(params) {
+    
+  }
+
+  export{displayName,$title,as get,$ex,$log};
+  
